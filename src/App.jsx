00e3bc135446fb1da0040bea8f27e41d50b4f6cd
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-01-10 10:04:40 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,22 @@ function App() {
 
   }, [length, Number, char, setpassword])
 
-  return (
-    <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg  my-8 text-red-500 bg-green-900 text-xl '>
-        password
-      </div>
-    </>
-  )
+  return ( 
+<>
+<div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-red-700 bg-white-500'>
+  <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+
+    <input type="text"
+    value={password}
+    className='outline-none w-full py-1 px-3 '
+    placeholder='Password'
+    readOnly
+    />
+  </div>
+  </div>
+
+</>
+)
 }
 
-export default App
+        export default App
