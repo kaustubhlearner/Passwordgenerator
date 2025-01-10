afc1d2c66684ec495ec8b636355ce8c1cc968853
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-01-10 10:25:59 +0530

    third-commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,22 +24,37 @@ function App() {
 
   }, [length, Number, char, setpassword])
 
-  return ( 
-<>
-<div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-red-700 bg-white-500'>
-  <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+  return (
+    <>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-red-700 bg-white-500'>
+        <h1 className='text-black text-center my-5'>Password Generator </h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
 
-    <input type="text"
-    value={password}
-    className='outline-none w-full py-1 px-3 '
-    placeholder='Password'
-    readOnly
-    />
-  </div>
-  </div>
+          <input type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3 '
+            placeholder='Password'
+            readOnly
+          />
+          <button className='text-white cursor-pointer bg-blue-700 w-15 '>copy</button>
+        </div>
+        <div className='flex  text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range"
+              min={3}
+              max={50}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setlength(e.target.value) }}
+            />
+            <label className='text-blue-700' > Length: {length}</label>
 
-</>
-)
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
 }
 
-        export default App
+export default App
